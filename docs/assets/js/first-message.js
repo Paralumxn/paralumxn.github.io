91e3dc0b0d15adf2c7333915a1195e52afbf9326
Baby Jean Baido <babybaido18@gmail.com>
2022-05-25 11:00:40 +0800

    modified

---
+++ docs/assets/js/first-message.js
@@ -0,0 +1,30 @@
+function hiddenMessage() {
+    swal.fire({
+        timer: 5000,
+        timerProgressBar: true,
+        html: "You found a hidden message!<br>Would you like to know the message?",
+        showConfirmButton: true,
+        showCancelButton: true,
+    }).then((result) => {
+        if(result.isConfirmed) {
+            swal.fire({
+                timerProgressBar: true,
+                timer: 2000,
+                allowEscapeKey: false,
+                allowOutsideClick: false,
+                icon: "info",
+                html: "You're now redirecting to the hidden message of this creator.",
+                showConfirmButton: false,
+                showCancelButton: false,
+                didOpen: () => {
+                    timerInterval = setInterval(() => {
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                    window.open("https://paralumxn.github.io/docs/assets/pages/second-message.html")
+                }
+            })
+        }
+    })
+}
